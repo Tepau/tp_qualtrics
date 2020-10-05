51abac9886cecd5a1855fe9b70193168228c7a56
Lemaitre_Salmon Malaury <malaury.lemaitresalmon@gmail.com>
2020-10-05 15:02:08 +0200

    remove bad task

--- app/tasks.py
+++ app/tasks.py
@@ -12,14 +12,3 @@ def create_new_contact(firstname, lastname, email):
                       data=json.dumps(body, indent=4))
     return 'Contact created with success!'
 
-
-@shared_task
-def contact_list():
-    my_list = []
-    headers = {'X-API-TOKEN': 'eCaTZTJ67f6folFZjPUvR5g1THGtcYFGSL8zhnlC'}
-    r = requests.get('https://fra1.qualtrics.com/API/v3/directories/POOL_3emqkuBXr2bdFQC/contacts', headers=headers)
-    for user in r.json()['result']['elements']:
-        if user['firstName'] is not None and user['lastName'] is not None and user['email'] is not None:
-            dico = {'prenom': user['firstName'], 'nom': user['lastName'], 'email': user['email']}
-            my_list.append(dico)
-    return dico
